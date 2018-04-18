f4e2032299733b20ea4d0f949ee91b3ff1389bf5
Vladislav Bushev <bushev98@list.ru>
2018-04-18 14:08:05 +0300

    hm7 second version

--- hm7/script.js
+++ hm7/script.js
@@ -1,14 +1,16 @@
-const b = document.getElementById('b');
+
+function scanDOM() {
+    const b = document.getElementById('b');
 
     var obj = {};
-    
-    for(var i =0; i< b.children.length; i++){
 
-        (obj[b.children[i].tagName])? obj[b.children[i].tagName] += 1: obj[b.children[i].tagName] =1;
-        (obj[b.children[i].className])? obj[b.children[i].className] +=1: obj[b.children[i].className] = 1;
-        (obj[b.children[i].textContent])? obj[b.children[i].textContent] +=1: obj[b.children[i].textContent] = 1;
-       
-        
+    for (var i = 0; i < b.children.length; i++) {
+
+        (obj[b.children[i].tagName]) ? obj[b.children[i].tagName] += 1 : obj[b.children[i].tagName] = 1;
+        (obj[b.children[i].className]) ? obj[b.children[i].className] += 1 : obj[b.children[i].className] = 1;
+        (obj[b.children[i].textContent]) ? obj[b.children[i].textContent] += 1 : obj[b.children[i].textContent] = 1;
+
+
     }
     obj['Текстовый узел'] = obj['some text'];
     delete obj['some text'];
@@ -16,4 +18,6 @@ const b = document.getElementById('b');
 
     for (const key in obj) {
         console.log(key + " : " + obj[key]);
-    }
+    };
+};
+scanDOM();
